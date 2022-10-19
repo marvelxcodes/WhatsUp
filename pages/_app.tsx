420df288457e67281ed87f5e0d39b5922dbfa8b7
marvelxcodes <ramakrishnan0046@gmail.com>
2022-10-19 23:15:22 +0530

    Intial Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import "@styles/main.scss"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
